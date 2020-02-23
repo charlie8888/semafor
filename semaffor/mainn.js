2cc09e03a500ade6b5b89762ca4847fe1e54e923
charlie8888 <56704015+charlie8888@users.noreply.github.com>
2020-02-23 19:43:54 +0100

    Add files via upload

---
+++ semaffor/mainn.js
@@ -0,0 +1,24 @@
+function semafor(){ 
+
+var boja = document.getElementsByClassName('boja');
+  boja[0].style.background = 'red';
+  boja[1].style.background = 'gray';
+  boja[2].style.background = 'gray';
+  
+  function changeToYellow(){
+
+	boja[1].style.background = 'yellow';
+	
+  }
+  function changeToGreen(){
+     boja[0].style.background = 'gray';
+     boja[1].style.background = 'gray';
+     boja[2].style.background = 'green';
+	  
+  }
+var x = setTimeout(changeToYellow, 4000);
+var y = setTimeout(changeToGreen, 8000);
+var start = setInterval(semafor,14000);
+
+}
+semafor();
